3fd3aea3198dd1973d4e0c469a76faae559e7cd0
沐沫 <lqllife@hotmail.com>
2025-12-01 15:38:44 +0800

    增加处理特殊文件名的情况

--- pkg/fileurl/file.go
+++ pkg/fileurl/file.go
@@ -43,13 +43,11 @@ func GetFileName(name string) string {
 }
 
 func GetFileNameOrRandom(fileName string) string {
-	// 通过剪切板上传的附件 都是一个默认名字
-	if fileName == "image.png" {
-		fileName = GetFileName(uuid.New().String() + fileName)
-	} else {
-		fileName = GetFileName(fileName)
-	}
-	return fileName
+    // 处理特殊文件名（image.png 和 Untitled开头的文件名）
+    if fileName == "image.png" || strings.HasPrefix(fileName, "Untitled") {
+        return GetFileName(uuid.New().String() + "-" + fileName)
+    }
+    return GetFileName(fileName)
 }
 
 // GetFileExt 获取文件后缀
